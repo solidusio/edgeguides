9877290fc52af07a724e2d1bd7c6614ab6df809d
Elia Schito <elia@schito.me>
2023-01-24 19:49:00 +0100

    Add dark mode support for the PRLink component
    - invert the PR image color in dark mode
    - use a regular image instead of a background
    - inherit the current link color instead of forcing it

--- src/theme/PRLink.js
+++ src/theme/PRLink.js
@@ -4,7 +4,8 @@ import PRIcon from '@site/static/img/pr.png';
 export default function PRLink(props) {
   return (
     <a class="pull_request" href={ `https://github.com/solidusio/solidus/pull/${ props.number }` }>
-      { props.description }
+      <img src={require('/static/img/pr.png').default}/>
+      { " " + props.description }
     </a>
   );
 };
